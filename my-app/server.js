d6b55ebeb7d3d1965834b8c64377a961f1d3ea5a
RaedShihab <raedshihab92@gmail.com>
2019-06-03 12:51:51 +0300

    starting with data base2

--- my-app/server.js
+++ my-app/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyparser = require('body-parser')
+const path = require('path')
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({
   extended: true
