b26d5082070c8c97da411f2264996d2857623147
RaedShihab <raedshihab92@gmail.com>
2019-06-03 21:47:59 +0300

    starting with data base5

--- my-app/routes/userRoute.js
+++ my-app/routes/userRoute.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../database');
+const User = require('../models/Model')
 
-router.get('/', (req, res) => res.send('Working'));
+router.get('/', (req,res)=>
+User.findAll()
+.then(user=>{
+    console.log(user);
+    res.sendStatus(200);
+})
+.catch(err=> console.log(err))
+);
 
 
 module.exports = router;  
